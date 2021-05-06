aab82a9765273bc570328a61456f97edf2b337ef
shivani jariwala <jariwalashivani1105@gmail.com>
2021-05-06 17:26:40 +0530

    test applied

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ app.use(express.urlencoded());
 
 app.use(router);
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
     console.log("Server is listening at http://localhost:3000");
 });
+
+module.exports = server
 
 
